8ab55f3e483e9ea7f16e341b1aa2ffb2fa78c953
erenMustafaOzdal <eren.060737@gmail.com>
2016-08-27 17:43:46 +0000

    izin

--- resources/views/partials/laravel-user-module/permissions_rate.blade.php
+++ resources/views/partials/laravel-user-module/permissions_rate.blade.php
@@ -1,7 +1,7 @@
 @inject('permission', 'ErenMustafaOzdal\LaravelUserModule\Services\PermissionService')
 
-<div class="easy-pie-chart tooltips" title="{{ lmcTrans('laravel-user-module/admin.fields.role.permission_rate', [ 'rate' => Sentinel::getUser()->is_super_admin ? 100 : $permission->permissionRate($model->permission_collect->count()) ]) }}">
-    <div class="number" data-percent="{!! Sentinel::getUser()->is_super_admin ? 100 : $permission->permissionRate($model->permission_collect->count()) !!}">
-        %<span>{!! Sentinel::getUser()->is_super_admin ? 100 : $permission->permissionRate($model->permission_collect->count()) !!}</span>
+<div class="easy-pie-chart tooltips" title="{{ lmcTrans('laravel-user-module/admin.fields.role.permission_rate', [ 'rate' => $model->is_super_admin ? 100 : $permission->permissionRate($model->permission_collect->count()) ]) }}">
+    <div class="number" data-percent="{!! $model->is_super_admin ? 100 : $permission->permissionRate($model->permission_collect->count()) !!}">
+        %<span>{!! $model->is_super_admin ? 100 : $permission->permissionRate($model->permission_collect->count()) !!}</span>
     </div>
 </div>
