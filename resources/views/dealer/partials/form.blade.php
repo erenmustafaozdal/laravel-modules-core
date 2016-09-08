bef40bdc4014c75ea9a1f60feb281b240acfc75c
erenMustafaOzdal <eren.060737@gmail.com>
2016-09-08 09:58:45 +0000

    dealer edit ve update işlemleri bitti

--- resources/views/dealer/partials/form.blade.php
+++ resources/views/dealer/partials/form.blade.php
@@ -32,7 +32,7 @@
 <div class="form-group">
     <label class="control-label">{!! lmcTrans('laravel-dealer-module/admin.fields.dealer.province_id') !!}</label>
     <select id="province_id" class="form-control form-control-solid placeholder-no-fix addresses" name="province_id" style="width: 100%">
-        @if(isset($dealer))
+        @if(isset($dealer) && ! is_null($dealer->province))
             <option value="{{ $dealer->province->id }}" selected>{{ $dealer->province->province }}</option>
         @endif
     </select>
@@ -50,8 +50,8 @@
 {{-- County --}}
 <div class="form-group">
     <label class="control-label">{!! lmcTrans('laravel-dealer-module/admin.fields.dealer.county_id') !!}</label>
-    <select id="county_id" class="form-control form-control-solid placeholder-no-fix addresses" name="county_id" disabled style="width: 100%">
-        @if(isset($dealer))
+    <select id="county_id" class="form-control form-control-solid placeholder-no-fix addresses" name="county_id" {{ ! isset($dealer) || ( is_null($dealer->county) || is_null($dealer->county->county) ) ? 'disabled' : '' }} style="width: 100%">
+        @if(isset($dealer) && ! is_null($dealer->county))
             <option value="{{ $dealer->county->id }}" selected>{{ $dealer->county->county }}</option>
         @endif
     </select>
@@ -69,8 +69,8 @@
 {{-- District --}}
 <div class="form-group">
     <label class="control-label">{!! lmcTrans('laravel-dealer-module/admin.fields.dealer.district_id') !!}</label>
-    <select id="district_id" class="form-control form-control-solid placeholder-no-fix addresses" name="district_id" disabled style="width: 100%">
-        @if(isset($dealer))
+    <select id="district_id" class="form-control form-control-solid placeholder-no-fix addresses" name="district_id" {{ ! isset($dealer) || ( is_null($dealer->district) || is_null($dealer->district->district) ) ? 'disabled' : '' }} style="width: 100%">
+        @if(isset($dealer) && ! is_null($dealer->district))
             <option value="{{ $dealer->district->id }}" selected>{{ $dealer->district->district }}</option>
         @endif
     </select>
@@ -88,8 +88,8 @@
 {{-- Neighborhood --}}
 <div class="form-group">
     <label class="control-label">{!! lmcTrans('laravel-dealer-module/admin.fields.dealer.neighborhood_id') !!}</label>
-    <select id="neighborhood_id" class="form-control form-control-solid placeholder-no-fix addresses" name="neighborhood_id" disabled style="width: 100%">
-        @if(isset($dealer))
+    <select id="neighborhood_id" class="form-control form-control-solid placeholder-no-fix addresses" name="neighborhood_id" {{ ! isset($dealer) || ( is_null($dealer->neighborhood) || is_null($dealer->neighborhood->neighborhood) ) ? 'disabled' : '' }} style="width: 100%">
+        @if(isset($dealer) && ! is_null($dealer->neighborhood))
             <option value="{{ $dealer->neighborhood->id }}" selected>{{ $dealer->neighborhood->neighborhood }}</option>
         @endif
     </select>
@@ -107,8 +107,8 @@
 {{-- Postal Code --}}
 <div class="form-group">
     <label class="control-label">{!! lmcTrans('laravel-dealer-module/admin.fields.dealer.postal_code_id') !!}</label>
-    <select id="postal_code_id" class="form-control form-control-solid placeholder-no-fix addresses" name="postal_code_id" disabled style="width: 100%" readonly="readonly">
-        @if(isset($dealer))
+    <select id="postal_code_id" class="form-control form-control-solid placeholder-no-fix addresses" name="postal_code_id" {{ ! isset($dealer) || ( is_null($dealer->postalCode) || is_null($dealer->postalCode->postal_code) ) ? 'disabled' : '' }} style="width: 100%" readonly="readonly">
+        @if(isset($dealer) && ! is_null($dealer->postalCode))
             <option value="{{ $dealer->postalCode->id }}" selected>{{ $dealer->postalCode->postal_code }}</option>
         @endif
     </select>
