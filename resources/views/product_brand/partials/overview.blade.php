88a326d27f4500e8c396c4ec3ff301190bb34427
erenMustafaOzdal <eren.060737@gmail.com>
2016-09-09 17:47:17 +0000

    show ve destroy işlemleri bitti

---
+++ resources/views/product_brand/partials/overview.blade.php
@@ -0,0 +1,40 @@
+{{-- Summary --}}
+<h1 class="font-blue sbold uppercase">{{ $product_brand->name_uc_first }}</h1>
+{{-- /Summary --}}
+
+{{-- Information on Form --}}
+<form class="form-horizontal" role="form" action="#">
+    {{-- Name --}}
+    <div class="form-group">
+        <label class="col-sm-2 control-label">
+            {!! lmcTrans('laravel-product-module/admin.fields.product_brand.name') !!}
+        </label>
+        <div class="col-sm-10">
+            <p class="form-control-static"> {{ $product_brand->name_uc_first }} </p>
+        </div>
+    </div>
+    {{-- /Name --}}
+
+    {{-- Created At --}}
+    <div class="form-group">
+        <label class="col-sm-2 control-label">
+            {!! trans('laravel-modules-core::admin.fields.created_at') !!}
+        </label>
+        <div class="col-sm-10">
+            <p class="form-control-static"> {{ $product_brand->created_at }} </p>
+        </div>
+    </div>
+    {{-- /Created At --}}
+
+    {{-- Updated At --}}
+    <div class="form-group">
+        <label class="col-sm-2 control-label">
+            {!! trans('laravel-modules-core::admin.fields.updated_at') !!}
+        </label>
+        <div class="col-sm-10">
+            <p class="form-control-static"> {{ $product_brand->updated_at }} </p>
+        </div>
+    </div>
+    {{-- /Updated At --}}
+</form>
+{{-- /Information on Form --}}
