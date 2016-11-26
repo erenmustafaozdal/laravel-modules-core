7622ba2e589d691ae2393f92b1b892d0cab51545
erenMustafaOzdal <eren.060737@gmail.com>
2016-11-26 15:02:06 +0000

    formlarda iptal butonu geri fonksiyonu oldu

--- resources/views/partials/form/actions.blade.php
+++ resources/views/partials/form/actions.blade.php
@@ -2,10 +2,12 @@
 <div class="form-actions {!! $type !!}">
     <div class="row">
         <div class="col-md-offset-3 col-md-9">
-            {!! Form::button( '<i class="fa fa-times"></i> <span class="hidden-xs">' . trans('laravel-modules-core::admin.ops.cancel') . '</span>', [
-                'class' => 'btn red btn-outline',
-                'type' => 'reset'
-            ]) !!}
+            <a href="{!! URL::previous() !!}" class="btn red btn-outline">
+                <i class="fa fa-times"></i>
+                <span class="hidden-xs">
+                    {!! trans('laravel-modules-core::admin.ops.cancel') !!}
+                </span>
+            </a>
             {!! Form::button( '<i class="fa fa-paper-plane"></i> <span class="hidden-xs">' . trans('laravel-modules-core::admin.ops.submit') . '</span>', [
                 'class' => 'btn blue btn-outline',
                 'type' => 'submit'
